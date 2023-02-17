012861e0616ed8573df0321d23b4f6b3e4b47408
shioncm <shioncm2001@gmail.com>
2023-02-17 13:40:06 -0500

    Success on inserting sample data to users table - successful insertion of data from testUsers.sql file with psql

---
+++ testUsers.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (user_name, password) VALUES ('Shion', 'software');
+INSERT INTO users (user_name, password) VALUES ('Alpha', 'password');
+INSERT INTO users (user_name, password) VALUES ('Beta', 'password');
+INSERT INTO users (user_name, password) VALUES ('Gamma', 'password');
+INSERT INTO users (user_name, password, total_games, total_win, total_loss, total_ties) VALUES ('Zeta', 'password1234', 5, 3, 1, 1);
